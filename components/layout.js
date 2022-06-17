fa0b0084e98c9fbf1726613bd88c01511556c87d
heartbeatSeries <2775142500@qq.com>
2022-06-17 11:14:04 +0800

    修改名称

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'weiping he';
+const name = 'heartbeatSeries';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
